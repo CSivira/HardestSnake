64af2ae65a2072f6493fdbc7cea9747ba95f5f7f
Carlos Sivira <carlos.csivira@gmail.com>
2018-08-01 21:37:10 -0400

    Fruta y menu

---
+++ Hardest Snake/Assets/Scripts/Fruit/FruitManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class FruitManager : MonoBehaviour {
+
+	public GameObject fruitPrefab;
+
+	private Vector3 initialFruit;
+	private GameObject[] actualFruits;
+	private float fruitCount;
+
+	void Start () {
+		initialFruit = new Vector3 (Random.Range (-4.5f,4.5f),Random.Range (-4.5f,4.5f), 0f);
+		Instantiate (fruitPrefab, initialFruit, Quaternion.identity);
+	}
+
+	void Update () {
+		fruitCount = GameObject.FindGameObjectsWithTag ("Fruit").Length;
+		actualFruits = GameObject.FindGameObjectsWithTag ("Fruit");
+
+		if (fruitCount == 0) {
+			CreateNewFruit ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			for (int i = 0; i < fruitCount; i++) {
+				Destroy (actualFruits[i]);
+			}
+		}
+	}
+
+	void CreateNewFruit() {
+		Vector3 newPosition = new Vector3 (Random.Range (-4.5f,4.5f),Random.Range (-4.5f,4.5f), 0f);
+		Instantiate (fruitPrefab, newPosition, Quaternion.identity);
+	}
+}
