2157b0b1f3dfe65c9263dfb9cd7a30f8a1979c15
Carlos Sivira <carlos.csivira@gmail.com>
2018-08-02 22:30:08 -0400

    Casi listo

---
+++ Hardest Snake/Assets/Scripts/FalseSnake/BodyPartMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class BodyPartMovement : MonoBehaviour {
+
+	public float speed;
+
+	private Transform target;
+	private Transform[] points;
+	private int index;
+	private float rotation;
+
+	void Start () {
+		points = new Transform[FalseRouteBehavior.points.Length];
+		points = FalseRouteBehavior.points;
+		index = 0;
+		target = points [index];
+	}
+
+	void Update () {
+		Vector3 direction = target.position - transform.position;
+		float distance = direction.magnitude;
+
+		if (distance <= 0.1f) {
+			GetNextPoint();
+			rotation = rotation + 90;
+			transform.rotation = Quaternion.Euler(0f,0f,rotation);
+		}
+		transform.Translate (direction.normalized * speed * Time.deltaTime, Space.World);
+	}
+
+	void GetNextPoint() {
+		if (index == points.Length - 1) {
+			index = 0;
+			target = points [index];
+		} else {
+			index++;
+			target = points [index];
+		}
+	}
+}
---
+++ Hardest Snake/Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public enum GameState {menu, playing, dead}
+
+	public GameObject menu;
+	public GameObject deadMenu;
+	public GameObject fruitManager;
+	public Text actualScore;
+	public Text bestScore;
+
+	private bool usserAction;
+	private GameState gameState = GameState.menu;
+	private int points;
+	private int bestPoints;
+
+	void Start () {
+		menu.SetActive (true);
+		deadMenu.SetActive (false);
+		fruitManager.SetActive (false);
+	}
+
+	void Update () {
+		usserAction = Input.GetKeyDown (KeyCode.Return);
+
+		if (gameState == GameState.menu) {
+			if (usserAction) {
+				menu.SetActive (false);
+				gameState = GameState.playing;
+				fruitManager.SetActive (true);
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			IngreasePoints ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && gameState == GameState.playing) {
+			gameState = GameState.dead;
+			deadMenu.SetActive (true);
+			fruitManager.SetActive (false);
+		}
+
+		if (gameState == GameState.dead) {
+			if (usserAction) {
+				RestartScene ();
+			}
+		}
+	}
+
+	void RestartScene () {
+		SceneManager.LoadScene ("MainScene");
+	}
+
+	void IngreasePoints() {
+		points++;
+		actualScore.text = points.ToString ();
+
+		if (bestPoints < points) {
+			SetBestScore ();
+		}
+	}
+
+	void SetBestScore() {
+		bestPoints = points;
+		bestScore.text = bestPoints.ToString ();
+	}
+}
